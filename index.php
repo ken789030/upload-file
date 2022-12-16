c08a54498882a31978f4b96a8ab8c6d88f46cb2b
ken <ken789030@gmail.com>
2022-12-16 10:23:25 +0800

    增加分析虧損CSV

--- index.php
+++ index.php
@@ -3,12 +3,73 @@
 use App\Contollers\Statistics;
 
 $jsonString = '';
+
+function csvToArray($filename, $delimiter = "\t") {
+    if (!file_exists($filename) || !is_readable($filename))
+        return false;
+
+    $header = '';
+    $data = [];
+    if (($fh = fopen($filename, 'r')) !== false) {
+        while (($csv = fgetcsv($fh, 1000, ',')) !== false) {
+            if (!$header)
+                $header = $csv;
+            else
+                $data[] = array_combine($header, $csv);
+        }
+    }
+
+    return $data;
+}
+    
+
+
 if (isset($_FILES) && $_FILES) {
     
     include_once dirname(__FILE__).'/vendor/autoload.php';
     include_once "app/Controllers/Statistics.php";
 
     $type = $_POST['type'];
+
+    if ($type == 'csv') {
+        $array = csvToArray($_FILES['file']['tmp_name'], ',');
+        $result = [];
+        foreach ($array as $value) {
+            if (!$result[$value['From']]) {
+                $result[$value['From']] = floatval($value['TxnFee(ETH)']);
+            } else {
+                $result[$value['From']] = floatval(bcadd($result[$value['From']],floatval($value['TxnFee(ETH)']), 9));
+            }
+        }
+
+        $format = [];
+        $count = 1;
+        $total = 0;
+        $fp = fopen('data.csv', 'a');
+    
+        // Append input data to the file  
+        
+        
+        // close the file
+        
+        foreach ($result as $key => $eth) {
+            $format[] = [
+                'No' => $count,
+                'From' => $key,
+                'ETH' => $eth
+            ];
+            fputcsv($fp, [
+                $count,
+                $key,
+                $eth
+            ]);
+            $total += $eth;
+            $count ++;
+        }
+        fclose($fp);
+        print_r(json_encode($format));
+        die;
+    }
 
     $jsonString = file_get_contents($_FILES['file']['tmp_name']);
     
@@ -73,6 +134,7 @@ if (isset($_FILES) && $_FILES) {
                     <option value="result">顯示分析結果</option>
                     <option value="excel">匯出Excel</option>
                     <option value="json">匯出json</option>
+                    <option value="csv">虧損csv</option>
                 </select>
             </form>
             <br>
